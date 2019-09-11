fe56560596fe7bda51bef0b52e48bc0b7f10f9d0
Gowtham0786 <54531133+Gowtham0786@users.noreply.github.com>
2019-09-11 11:25:57 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,6 +4,6 @@ public static void main(String[] args)
   {
     System.out.println("Hello World Program");
     System.out.println("Test Line of Codes");
-    System.out.println("Push Event");  // Sample Commit
+    System.out.println("Push Event for Jenkins Build Trigger");  // Sample Commit
   }
 }
